41dbd5f1f3b5918e9d99787687956bf55669d463
Craig McGowan <mcgowan.cj@gmail.com>
2018-07-05 08:23:38 +0200

    Investigate adding polynomial features to models of 4 most recent match form

--- R/run_markdown.R
+++ R/run_markdown.R
@@ -1,3 +1,7 @@
 rmarkdown::render("R/prem_sing_feat_result.Rmd",
                   output_format = "html_document",
-                  output_file = "Results/Prem_Single_Feature_Results.html")
+                  output_dir = "Results/")
+
+rmarkdown::render("R/prem_sing_poly_feat_result.Rmd",
+                  output_format = "html_document",
+                  output_dir = "Results/")
