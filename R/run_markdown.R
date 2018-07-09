f709e2c38a0103d05d68407a19b79378038bd5bd
Craig McGowan <mcgowan.cj@gmail.com>
2018-07-09 14:09:00 +0200

    File renaming and initial commit of first SVM attempt

--- R/run_markdown.R
+++ R/run_markdown.R
@@ -1,7 +1,11 @@
-rmarkdown::render("R/prem_sing_feat_result.Rmd",
+rmarkdown::render("R/prem_sing_feat_logistic.Rmd",
                   output_format = "html_document",
                   output_dir = "Results/")
 
-rmarkdown::render("R/prem_sing_poly_feat_result.Rmd",
+rmarkdown::render("R/prem_sing_poly_feat_logistic.Rmd",
+                  output_format = "html_document",
+                  output_dir = "Results/")
+
+rmarkdown::render("R/prem_mult_feat_logistic.Rmd",
                   output_format = "html_document",
                   output_dir = "Results/")
